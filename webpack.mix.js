389eeef574fb7d91935a9ed39babc62274183075
Duoli <zhaody901@126.com>
2018-11-24 01:02:48 +0800

    update webpack, remove fe,graphql, bower, update composer, package dependcy

--- webpack.mix.js
+++ webpack.mix.js
@@ -16,23 +16,61 @@ var mix = require('laravel-mix');
  | file for the application as well as bundling up all the JS files.
  |
  */
-mix.sass(
-	'resources/assets/scss/basic.scss',
-	'public/resources/css'
-).sass(
-	'resources/assets/scss/app.scss',
-	'public/resources/css'
-).sass(
-	'resources/assets/scss/web.scss',
-	'public/resources/css'
-).sass(
-	'modules/system/resources/scss/develop/style.scss',
-	'public/resources/css/develop.css'
-).sass(
-	'modules/system/resources/scss/backend/style.scss',
-	'public/resources/css/backend.css'
-);
-
-mix.copyDirectory('modules/user/resources/images', 'public/modules/user/images');
-mix.copyDirectory('modules/order/resources/images', 'public/modules/order/images');
-mix.copyDirectory('modules/web/resources/images', 'public/modules/web/images');
+mix
+/*
+|--------------------------------------------------------------------------
+| develop & backend
+|--------------------------------------------------------------------------
+*/
+/* system js
+ * ---------------------------------------- */
+	.combine([
+			'modules/system/resources/libs/jquery/2.2.4/jquery.min.js',
+			'modules/system/resources/libs/jquery/form/jquery.form.js',
+			'modules/system/resources/libs/jquery/toastr/jquery.toastr.js',
+			'modules/system/resources/libs/jquery/pjax/jquery.pjax.js',
+			'modules/system/resources/libs/jquery/poshytip/jquery.poshytip.js',
+			'modules/system/resources/libs/jquery/validation/jquery.validation.js',
+			'modules/system/resources/libs/jquery/data-tables/jquery.data-tables.js',
+			'modules/system/resources/libs/jquery/tokenize2/jquery.tokenize2.js',
+			'modules/system/resources/libs/waves/waves.min.js',
+			'modules/system/resources/libs/vue/vue.min.js',
+			'modules/system/resources/libs/underscore/underscore.js',
+			'modules/system/resources/libs/jshash/md5.min.js',
+			'modules/system/resources/libs/jshash/sha1.min.js',
+			'modules/system/resources/libs/clipboard/clipboard.min.js',
+			'modules/system/resources/libs/wang-editor/wangEditor.js',
+			'modules/system/resources/libs/bt3/js/tooltip.js',
+			'modules/system/resources/libs/jquery/fancybox/jquery.fancybox.min.js',
+		],
+		'public/assets/js/system_vendor.js'
+	)
+	.combine([
+			'modules/system/resources/libs/poppy/util.js',
+			'modules/system/resources/libs/poppy/cp.js',
+			'modules/system/resources/libs/poppy/system/cp.js'
+		],
+		'public/assets/js/system_cp.js'
+	)
+	/* system css
+	 * ---------------------------------------- */
+	.sass(
+		'modules/system/resources/scss/system.scss',
+		'public/assets/css/system.css'
+	)
+	.copyDirectory('modules/system/resources/libs/layui', 'public/assets/layui')
+	.copyDirectory('modules/system/resources/images/libs', 'public/assets/images/libs')
+	.copyDirectory('modules/system/resources/fonts/roboto', 'public/assets/font/roboto')
+	.copyDirectory('modules/system/resources/fonts/fontawesome', 'public/assets/font/fontawesome')
+	.browserSync({
+		proxy : 'http://dev.play.com/',
+		files : [
+			"public/resources/js/**/*.js",
+			"public/resources/css/*.css",
+			"modules/**/resources/views/**/*.blade.php",
+			"modules/**/resources/mixes/**/*.js",
+			"modules/**/resources/js/**/*.js"
+		]
+	})
+	.disableNotifications()
+	.version();
